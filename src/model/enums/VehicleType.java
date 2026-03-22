480110d565dd2f025e53077779cc667d6c5f07f6
Sachidaanand Mondal <sachidaa55664@gmail.com>
2026-03-22 23:40:50 +0530

    Increased vehicles parameter and give type based size allotments

--- src/model/enums/VehicleType.java
+++ src/model/enums/VehicleType.java
@@ -1,7 +1,31 @@
 package model.enums;
 
+import model.enums.VehicleSize;
+
+
 public enum VehicleType {
-    NORMAL,
-    VIP,
-    DISABLED
+    NORMAL(2,false,null),
+    VIP(6,false,null),
+    DISABLED(8,true,VehicleSize.STANDARD),
+    AMBULANCE(10,true,VehicleSize.LARGE);
+
+    private final int priority;
+    private final boolean isFixedSize;
+    private final VehicleSize fixedSize;
+
+    VehicleType(int priority,boolean isFixedSize,VehicleSize fixedSize){
+        this.priority= priority;
+        this.isFixedSize=isFixedSize;
+        this.fixedSize=fixedSize;
+    }
+
+    public int getPriority(){return priority;}
+    public boolean isFixedSize(){return isFixedSize;}
+    public VehicleSize getFixedSize(){return fixedSize;}
+
+    public VehicleSize resolveSize(VehicleSize generatedSize){
+        if(isFixedSize){return fixedSize;}
+        else return generatedSize;
+    }
+
 }
