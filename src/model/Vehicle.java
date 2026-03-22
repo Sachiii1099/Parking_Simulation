a70a8e07798f8c200e53599835be12ee1dfa0708
Sachidaanand Mondal <sachidaa55664@gmail.com>
2026-03-22 23:42:28 +0530

    Added sizes for parking slots and also handle exception if occupied or not

--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -1,17 +1,21 @@
 package model;
 
 import model.enums.VehicleType;
+import model.enums.VehicleSize;
 
 public class Vehicle {
     private int id;
     private VehicleType type;
     private Cell entryGate;
-    public Vehicle(int id,VehicleType type,Cell entry){
+    private VehicleSize size;
+    public Vehicle(int id,VehicleType type,Cell entryGate,VehicleSize size){
         this.id=id;
         this.type=type;
         this.entryGate = entryGate;
+        this.size=type.resolveSize(size);
     }
     public int getId(){return id;}
+    public VehicleSize getSize(){ return size;}
     public VehicleType getType(){return type;}
     public Cell getEntryGate(){return entryGate;}
 
