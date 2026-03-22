c38dc0c62be79e37d7061a10654bd0c93ca263a9
Sachidaanand Mondal <sachidaa55664@gmail.com>
2026-03-22 23:45:25 +0530

    Upgraded the imput technique with slot size input and gate check

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,5 +1,7 @@
 package core;
 import model.Parking;
+import model.Cell;
+import model.enums.VehicleSize;
 import model.enums.CellType;
 import java.util.*;
 
@@ -14,29 +16,53 @@ public class Main {
         System.out.print("Enter number of columns");
         cols= sc.nextInt();
 
-        CellType[][] layout= new CellType[rows][cols];
+        Parking p;
+        do {
+            Cell[][] grid = new Cell[rows][cols];
 
-    System.out.println("Customize your parking");
-    System.out.println("Enter as per the codes:\n 0--ROAD \n 1--SLOT \n 2--GATE \n 3--BLOCK\n 4--LIFT\n");
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<cols;j++){
-            int input= sc.nextInt();
+            System.out.println("Customize your parking");
+            System.out.println("Enter as per the codes:\n 0--ROAD \n 1--SLOT  (enter size: 1=STANDARD, 2=LARGE) \n 2--GATE \n 3--BLOCK\n 4--LIFT\n");
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    int input = sc.nextInt();
 
-            switch(input){
-                case 0: layout[i][j]=CellType.ROAD; break;
-                case 1: layout[i][j]=CellType.SLOT; break;
-                case 2: layout[i][j]=CellType.GATE; break;
-                case 3: layout[i][j]=CellType.BLOCK; break;
-                case 4: layout[i][j]=CellType.LIFT; break;
-                default: layout[i][j]=CellType.ROAD; break;
+                    switch (input) {
+                        case 0:
+                            grid[i][j] = new Cell(i, j, CellType.ROAD);
+                            break;
+                        case 1: {
+                            int sizeEnter = sc.nextInt();
+                            VehicleSize size;
+                            if (sizeEnter == 2) {
+                                size = VehicleSize.LARGE;
+                            } else {
+                                size = VehicleSize.STANDARD;
+                            }
+                            grid[i][j] = new Cell(i, j, CellType.SLOT, size);
+                            break;
+                        }
+                        case 2:
+                            grid[i][j] = new Cell(i, j, CellType.GATE);
+                            break;
+                        case 3:
+                            grid[i][j] = new Cell(i, j, CellType.BLOCK);
+                            break;
+                        case 4:
+                            grid[i][j] = new Cell(i, j, CellType.LIFT);
+                            break;
+                        default:
+                            grid[i][j] = new Cell(i, j, CellType.ROAD);
+                            break;
+                    }
+
+                }
             }
-        }
-    }
-    Parking p=new Parking(rows,cols,layout);
-    if(p.getGates().isEmpty()){
-        System.out.println("No gates are present, Please enter gates");
+            p=new Parking(rows,cols,grid);
+            if(p.getGates().isEmpty()){
+                System.out.println("No gates are present, Please enter gates");
 
-    }
+            }
+        }while(p.getGates().isEmpty());
 
 
 
