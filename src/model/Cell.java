431607528934e18f70213cc5fe1edd1b09e6bb73
Sachidaanand Mondal <sachidaa55664@gmail.com>
2026-03-22 23:44:18 +0530

    added the resolving size factor same as the vehicle model

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,21 +1,37 @@
 package model;
 import model.enums.CellType;
+import model.enums.VehicleSize;
+
+
+
 public class Cell {
     private int row;
     private int col;
     private CellType type;
     private boolean occupied;
+    private VehicleSize slotSize;
 
     public Cell(int row,int col,CellType type){
         this.row= row;
         this.col=col;
         this.type = type;
+        this.slotSize=VehicleSize.STANDARD;
+
+
+    }
+    public Cell(int row,int col,CellType type,VehicleSize slotSize){
+        this.row= row;
+        this.col=col;
+        this.type = type;
+        this.slotSize=slotSize;
+
     }
 
     public int getRow(){return row;}
     public int getCol(){return col;}
     public CellType getType(){return type;}
     public boolean isOccupied(){return occupied;}
+    public VehicleSize getSlotSize(){return slotSize;}
 
     public void setType(CellType type){
         this.type=type;
@@ -23,6 +39,8 @@ public class Cell {
     public void setOccupied(boolean occupied){
         if(this.type==CellType.SLOT) {
             this.occupied = occupied;
+        }else {
+            throw new IllegalStateException("Occupied is checked on a wrong cell which is ["+row+"] ["+col+"].");
         }
     }
 
