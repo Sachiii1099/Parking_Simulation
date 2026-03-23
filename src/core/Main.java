ea086bd060b0fb9d904e34450f022ed2e7533ae6
Sachidaanand Mondal <sachidaa55664@gmail.com>
2026-03-24 04:25:49 +0530

    Changedd the parking insertion approach along with print it fron every tick

--- src/core/Main.java
+++ src/core/Main.java
@@ -3,6 +3,8 @@ import model.Parking;
 import model.Cell;
 import model.enums.VehicleSize;
 import model.enums.CellType;
+import simulation.WorkingSimulation;
+
 import java.util.*;
 
 public class Main {
@@ -21,7 +23,7 @@ public class Main {
             Cell[][] grid = new Cell[rows][cols];
 
             System.out.println("Customize your parking");
-            System.out.println("Enter as per the codes:\n 0--ROAD \n 1--SLOT  (enter size: 1=STANDARD, 2=LARGE) \n 2--GATE \n 3--BLOCK\n 4--LIFT\n");
+            System.out.println("Enter as per the codes:\n 0--ROAD \n 1--SLOT  (enter size: 6=STANDARD, 7=LARGE) \n 2--GATE \n 3--BLOCK\n 4--LIFT\n");
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
                     int input = sc.nextInt();
@@ -33,7 +35,7 @@ public class Main {
                         case 1: {
                             int sizeEnter = sc.nextInt();
                             VehicleSize size;
-                            if (sizeEnter == 2) {
+                            if (sizeEnter == 7) {
                                 size = VehicleSize.LARGE;
                             } else {
                                 size = VehicleSize.STANDARD;
@@ -64,6 +66,16 @@ public class Main {
             }
         }while(p.getGates().isEmpty());
 
+        System.out.println("Enter the time parking is open");
+        int clock=sc.nextInt();
+
+        System.out.println("\n-=-=-=-=-=-=-=-=-=-WELCOME-=-=-=-=-=-=-==-=-=");
+        WorkingSimulation sim=new WorkingSimulation(p);
+        sim.printParking(p);
+        for(int i=0;i<clock;i++){
+            System.out.println("*******-------*******Clock"+(i+1)+"*********--------*********");
+            sim.runClock();
+        }
 
 
     }
