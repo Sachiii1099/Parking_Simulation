846b3b58e7763b8bf9a1a6eefa4392ca94694891
Sachidaanand Mondal <sachidaa55664@gmail.com>
2026-04-08 12:27:38 +0530

    improved the following in the slot allocation process firsty checks take floor initially and then put the search logic and added that car can arrive form any lift based on the nearest slot form lift such lift or gate is allocated for the second floor

--- src/AllPerformingFunctions/SlotAllocation.java
+++ src/AllPerformingFunctions/SlotAllocation.java
@@ -1,12 +1,15 @@
-package service;
+package AllPerformingFunctions;
 
-import model.Cell;
-import model.Parking;
-import model.ParkingFloor;
-import model.Vehicle;
-import model.enums.VehicleSize;
-import model.enums.CellType;
-import model.enums.VehicleType;
+import BasicBuildingBlocks.Cell;
+import BasicBuildingBlocks.Parking;
+
+
+
+import BasicBuildingBlocks.ParkingFloor;
+    import BasicBuildingBlocks.Vehicle;
+import BasicBuildingBlocks.enums.VehicleSize;
+            import BasicBuildingBlocks.enums.CellType;
+            import BasicBuildingBlocks.enums.VehicleType;
 import java.util.*;
 
 
@@ -17,39 +20,63 @@ public class SlotAllocation {
 
         for (int f = 0; f < building.getTotalFloors(); f++) {
             ParkingFloor currentFloor = building.getFloor(f);
-            Cell best = null;
+                             Cell best = null;
             int minDist = Integer.MAX_VALUE;
 
             System.out.println("VehicleID of       " + car.getId() + " " + car.getType() +" of size "+car.getSize()+ " is check ing Floor " + f + "...");
 
+
+
+
             for (int i = 0; i < currentFloor.getRows(); i++) {
-                for (int j = 0; j < currentFloor.getCols(); j++) {
-                    Cell cell = currentFloor.getGrid()[i][j];
+                        for (int j = 0; j < currentFloor.getCols(); j++) {
+                            Cell cell = currentFloor.getGrid()[i][j];
+
+
+
 
                     if (cell.getType() == CellType.SLOT && !cell.isOccupied() && isVehicleValid(cell, car, currentFloor)) {
 
                         List<Cell> gatesToCheck = new ArrayList<>();
+
+
                         if (f == 0) {
                             // Floor 0: Must enter through the specific gate the car arrived at
-                            gatesToCheck.add(gate);
-                        } else {
+                                gatesToCheck.add(gate);
+                        } else
+                        {
+
+
+
                             // Upper floors: Car can arrive via ANY lift/gate on this floor
                             gatesToCheck.addAll(currentFloor.getGates());
                         }
+
 
                         for (Cell startCell : gatesToCheck) {
                             List<Cell> path = parkingService.findPath(currentFloor, startCell, cell);
 
                             if (!path.isEmpty()) {
-                                int dist = path.size();
+                                             int dist = path.size();
                                 if (dist < minDist) {
                                     minDist = dist;
+
+
                                     best = cell;
                                 }
+
+
                             }
+
+
                         }
+
                     }
-                }
+
+
+
+
+               }
             }
 
             if (best != null) {
@@ -64,14 +91,21 @@ public class SlotAllocation {
     }
 
     private boolean isVehicleValid(Cell cell, Vehicle car, ParkingFloor area){
+
         if(car.getType()==VehicleType.AMBULANCE  ){
-            return cell.getSlotSize() == VehicleSize.LARGE;
+
+
+
+
+                      return cell.getSlotSize() == VehicleSize.LARGE;
         }
 
 
         if(car.getType()==VehicleType.DISABLED ){
+
+
             return cell.getSlotSize() == VehicleSize.STANDARD
-                    && isLiftPresent(cell, area);
+                                                                 && isLiftPresent(cell, area);
         }
 
 
@@ -81,20 +115,28 @@ public class SlotAllocation {
 
     private boolean isLiftPresent(Cell cell, ParkingFloor area){
         int r=cell.getRow();
-        int c=cell.getCol();
+                                                         int c=cell.getCol();
 
         int radius = 2;
 
         for(int i = r - radius; i <= r + radius; i++) {
             for(int j = c - radius; j <= c + radius; j++) {
-                if(i >= 0 && i < area.getRows()
-                        && j >= 0 && j < area.getCols()) {
-                    if(area.getGrid()[i][j].getType() == CellType.LIFT) {
+                       if(i >= 0 && i < area.getRows()
+                             && j >= 0 && j < area.getCols()) {
+                                 if(area.getGrid()[i][j].getType() == CellType.LIFT) {
+
+
                         return true;
                     }
+
+
                 }
             }
+
+
+
         }
         return false;
+
     }
 }
