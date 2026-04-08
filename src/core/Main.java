bbda4c69e457072f2590896aa3a323331a621d39
Sachidaanand Mondal <sachidaa55664@gmail.com>
2026-04-08 12:32:34 +0530

    added floor input and along wiht it take input for first floor in addition with the ground floor and then apply the same first floor layout for the rest of the floors

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,34 +1,49 @@
 package core;
-import model.Parking;
-import model.Cell;
-import model.enums.VehicleSize;
-import model.enums.CellType;
-import simulation.WorkingSimulation;
+import BasicBuildingBlocks.Parking;
+import BasicBuildingBlocks.Cell;
+import BasicBuildingBlocks.ParkingFloor;
+import BasicBuildingBlocks.enums.VehicleSize;
+import BasicBuildingBlocks.enums.CellType;
+import logicOfSimulation.WorkingSimulation;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        int rows,cols;
-
-
+        int rows, cols, numFloors;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of rows:");
-        rows= sc.nextInt();
-        System.out.print("Enter number of columns");
-        cols= sc.nextInt();
 
-        Parking p;
+        System.out.print("Enter number of rows: ");
+        rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        cols = sc.nextInt();
+
+
+
+
+        System.out.print("Enter total number of floors: ");
+        numFloors = sc.nextInt();
+
+        List<ParkingFloor> allFloors = new ArrayList<>();
+
+
+        boolean basementValid = false;
         do {
             Cell[][] grid = new Cell[rows][cols];
+            System.out.println("Input for the ground floor...");
 
-            System.out.println("Customize your parking");
-            System.out.println("Enter as per the codes:\n 0--ROAD \n 1--SLOT  (enter size: 6=STANDARD, 7=LARGE) \n 2--GATE \n 3--BLOCK\n 4--LIFT\n");
+
+            System.out.println("Enter \n 0--ROAD \n 1--SLOT  (enter size: 6=STANDARD, 7=LARGE) \n 2--GATE \n 3--BLOCK\n 4--LIFT\n");
+
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
                     int input = sc.nextInt();
-
                     switch (input) {
+
+
+
+
                         case 0:
                             grid[i][j] = new Cell(i, j, CellType.ROAD);
                             break;
@@ -39,6 +54,11 @@ public class Main {
                                 size = VehicleSize.LARGE;
                             } else {
                                 size = VehicleSize.STANDARD;
+
+
+
+
+
                             }
                             grid[i][j] = new Cell(i, j, CellType.SLOT, size);
                             break;
@@ -54,30 +74,143 @@ public class Main {
                             break;
                         default:
                             grid[i][j] = new Cell(i, j, CellType.ROAD);
+
+
+
+
+
                             break;
                     }
-
                 }
             }
-            p=new Parking(rows,cols,grid);
-            if(p.getGates().isEmpty()){
-                System.out.println("No gates are present, Please enter gates");
+
+
+            boolean hasGate = false;
+                     for(int i = 0; i < rows; i++) {
+                for(int j = 0; j < cols; j++) {
+                      if(grid[i][j].getType() == CellType.GATE) hasGate = true;
+                }
+            }
+
+            if(!hasGate) {
+                             System.out.println("No gates are present in basement, Please enter gates");
+            } else {
+                allFloors.add(new ParkingFloor(0, rows, cols, grid));
+                           basementValid = true;
+
+
+
+
+
 
             }
-        }while(p.getGates().isEmpty());
+        } while(!basementValid);
 
-        System.out.println("Enter the time parking is open");
-        int clock=sc.nextInt();
 
-        System.out.println("\n-=-=-=-=-=-=-=-=-=-WELCOME-=-=-=-=-=-=-==-=-=");
-        WorkingSimulation sim=new WorkingSimulation(p);
-        sim.printParking(p);
-        for(int i=0;i<clock;i++){
-            System.out.println("*******-------*******Clock"+(i+1)+"*********--------*********");
+        if (numFloors > 1) {
+            Cell[][] floor1Grid = new Cell[rows][cols];
+            System.out.println("Input for the 1st floor ");
+
+
+
+                 System.out.println("0--ROAD, 1--SLOT (6/7), 2--GATE, 3--BLOCK, 4--LIFT");
+            for (int i = 0; i < rows; i++) {
+
+
+
+                for (int j = 0; j < cols; j++) {
+                    int input = sc.nextInt();
+                    switch (input) {
+                        case 0:
+                            floor1Grid[i][j] = new Cell(i, j, CellType.ROAD);
+                            break;
+                        case 1: {
+                            int sizeEnter = sc.nextInt();
+                            VehicleSize size;
+                            if (sizeEnter == 7) {
+                                size = VehicleSize.LARGE;
+                            } else {
+                                size = VehicleSize.STANDARD;
+                            }
+                            floor1Grid[i][j] = new Cell(i, j, CellType.SLOT, size);
+                            break;
+                                        }
+                                        case 2:
+                                            floor1Grid[i][j] = new Cell(i, j, CellType.GATE);
+                                            break;
+                                        case 3:
+                                            floor1Grid[i][j] = new Cell(i, j, CellType.BLOCK);
+                                            break;
+                                        case 4:
+                                            floor1Grid[i][j] = new Cell(i, j, CellType.LIFT);
+                                            break;
+                        default:
+                            floor1Grid[i][j] = new Cell(i, j, CellType.ROAD);
+                            break;
+
+
+
+
+                    }
+
+
+
+                }
+
+            }
+            allFloors.add(new ParkingFloor(1, rows, cols, floor1Grid));
+
+            // Copy Floor 1 layout for remaining floors
+            for (int f = 2; f < numFloors; f++) {
+
+
+
+
+
+                allFloors.add(new ParkingFloor(f, rows, cols, copyLayout(rows, cols, floor1Grid)));
+            }
+        }
+
+        Parking p = new Parking(allFloors);
+
+                    System.out.println("Enter the time parking is open");
+                    int clock = sc.nextInt();
+
+                    System.out.println("\nStarting Building Simulation-----------------");
+                          WorkingSimulation sim = new WorkingSimulation(p);
+
+
+
+
+                    for(int i = 0; i < clock; i++){
+            System.out.println("Clock " + (i+1) + "----------------");
             sim.runClock();
         }
+    }
+
+    private static Cell[][] copyLayout(int rows, int cols, Cell[][] original) {
+        Cell[][] copy = new Cell[rows][cols];
+
+
+
+
+        for (int i = 0; i < rows; i++) {
+             for (int j = 0; j < cols; j++) {
+                                     copy[i][j] = new Cell(i, j, original[i][j].getType(), original[i][j].getSlotSize());
+                            }
+
+
+
+
+
+
+        }
+        return copy;
+
+
+
+
 
 
     }
-
 }
